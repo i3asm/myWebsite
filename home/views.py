702446f6a232aadc5dc79641aa6e6589a0e960f4
i3asm <i3asmmail@gmail.com>
2020-06-07 20:57:32 +0300

    html and styles from carrd.co

--- home/views.py
+++ home/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse, HttpResponseNotFound
-from getenv import env
-from github import Github
+from django.shortcuts import render
 
 
 def index(request):
-
-    g = Github(env('GITHUB'))
-    for repo in g.get_user().get_repos():
-        return HttpResponse('<h1>hello' + repo + 'world</h1>')
+    context = {
+        'none': None
+    }
+    return render(request, 'home/index.html', context)
